918c7d55adda2f6034da11ecf8e98dad1646433e
DanielBenBassat <daniel.benbassat@gmail.com>
2024-02-28 19:08:29 +0200

    final

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-import socket
+from scapy.all import *
 
-IP = '127.0.0.1'
+
+IP_ADD = "127.0.0.1"
 START_PORT = 20
 END_PORT = 1024
+TIMEOUT = 0.5
 
 
-def scan_ports(remote_host, start_port, end_port):
-    open_ports = []
-    timeout = 0.5
+def scan_ports(start_port, end_port):
 
     for port in range(start_port, end_port+1):
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(timeout)
-        result = sock.connect_ex((remote_host, port))
-        if result == 0:
-            open_ports.append(port)
-        sock.close()
-    return open_ports
+        syn_packet = IP(dst=IP_ADD)/TCP(dport=port, flags="S")
+        response = sr1(syn_packet, timeout=TIMEOUT, verbose=0)
+        # If response is none or has RST flag port is closed
+        if response is None:
+            print(".")
+        elif TCP in response and response[TCP].flags & 0x04:
+            print(".")
+        # If responsehas SYN+ACK flags, port is open
+        elif TCP in response and response[TCP].flags & 0x12:
+            print(f"Port {port} is open.")
+        else:
+            print(".")
 
 
 def main():
-    open_ports = scan_ports(IP, START_PORT, END_PORT)
-    if open_ports:
-        print("Open ports:")
-        for port in open_ports:
-            print(port)
-    else:
-        print("No open ports found.")
+    scan_ports(START_PORT, END_PORT)
 
 
 if __name__ == '__main__':
     main()
+
 
